fb32a73db05e1decf43d2ba8e7463189a14c23c1
vinchamp77 <vinchamp77@gmail.com>
2022-08-15 11:12:39 +1000

    Clean up imports

--- app/src/main/java/com/example/emptycomposeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/emptycomposeapp/ui/MainActivity.kt
@@ -3,11 +3,6 @@ package com.example.emptycomposeapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.emptycomposeapp.R
 import com.example.emptycomposeapp.ui.screens.MainScreen
 import com.example.emptycomposeapp.ui.theme.EmptyComposeAppTheme
 
