fca3fb31ff1498b9fe8e84571c24365bd43903db
vinchamp77 <vinchamp77@gmail.com>
2022-09-11 18:58:07 +1000

    Add androidx.lifecycle:lifecycle-viewmodel-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,10 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    val lifeCycleVersion = "2.5.1"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifeCycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion")
+
     implementation("androidx.activity:activity-compose:1.5.1")
 
     val composeVersion = "1.2.1"
@@ -60,5 +63,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.24.2-alpha")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.24.10-beta")
 }
