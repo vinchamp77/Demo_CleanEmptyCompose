009ffa343824ddc9d5f2474bdb31b130312931fd
vinchamp77 <vinchamp77@gmail.com>
2022-10-02 15:01:07 +1100

    Upgrade libraries and targetSdk/compileSdk to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "vtsen.hashnode.dev.newemptycomposeapp"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -50,12 +50,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     val lifeCycleVersion = "2.5.1"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifeCycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion")
 
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.activity:activity-compose:1.6.0")
 
     val composeVersion = "1.2.1"
     implementation("androidx.compose.ui:ui:$composeVersion")
