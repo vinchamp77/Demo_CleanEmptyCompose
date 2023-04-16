9da175f92e37618712a21a7e348a5f4cdba2b3a7
vinchamp77 <vinchamp77@gmail.com>
2023-04-16 12:42:26 +1000

    Upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     }
     composeOptions {
         //https://developer.android.com/jetpack/androidx/releases/compose-compiler
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.5"
     }
     packaging {
         resources {
@@ -51,19 +51,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
 
-    implementation("androidx.compose.ui:ui:1.4.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0")
+    implementation("androidx.compose.ui:ui:1.4.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.1")
 
-    implementation("androidx.compose.material3:material3:1.1.0-beta01")
+    implementation("androidx.compose.material3:material3:1.1.0-beta02")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
     implementation("androidx.core:core-splashscreen:1.0.0")
 
-    implementation("com.github.vinchamp77:buildutils:0.0.6")
+    implementation("com.github.vinchamp77:buildutils:0.0.8")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
     //https://kotlinlang.org/docs/releases.html#update-to-a-new-release
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
