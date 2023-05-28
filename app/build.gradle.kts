4da534bfc6cd7a0bd51d0ef767689f9c6a845ee1
vinchamp77 <vinchamp77@gmail.com>
2023-05-28 16:27:24 +1000

    AGP 8.0.2, upgrade libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,20 +51,20 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
 
     implementation("androidx.compose.ui:ui:1.4.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
     debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
 
-    implementation("androidx.compose.material3:material3:1.1.0-rc01")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
     implementation("androidx.core:core-splashscreen:1.0.1")
 
-    implementation("com.github.vinchamp77:buildutils:0.0.8")
+    implementation("com.github.vinchamp77:buildutils:0.0.11")
 }
