327661cda4e5c29fb819f1e539d819d01fb0a62c
vinchamp77 <vinchamp77@gmail.com>
2024-07-19 20:27:22 +1000

    Upgrade and remove the build workaround bug:

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 buildscript {
 }
 
-// Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
-// 'val Project.libs: LibrariesForLibs' can't be called in this context by implicit receiver. Use the explicit one if necessary
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -11,17 +8,12 @@ plugins {
     alias(libs.plugins.diffplug.spotless) apply false
 }
 
-/* Looks like this is not needed anymore
-//Workaround for "Expecting an expression" build error
-println("")
-*/
-
 subprojects {
     apply(plugin = "com.diffplug.spotless")
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         kotlin {
             target("**/*.kt")
-            targetExclude("$buildDir/**/*.kt")
+            targetExclude("${layout}.getBuildDirectory()/**/*.kt")
 
             ktlint()
             licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
